97d2fa3c1db95b8be755e3d9b746195f2faf0677
Rafael Soares Pinto <rsoaresp@gmail.com>
2018-06-11 20:27:11 -0300

    Update Erat.go
    Include some simplifications in the code.

--- Erat.go
+++ Erat.go
@@ -1,15 +1,17 @@
+package Erat
+
 import "math"
 
+// Implementation of the sieve of Eratosthenes
 func Erat(n int) []bool {
 
 	// We begin with all numbers marked as primes,
 	// since later we will delete all the multiples
 	// of previously know primes, starting from 2.
 	x := make([]bool, n)
-	for i:=0;i<n;i++ {
+	for i := 0; i < n; i++ {
 		x[i] = true
 	}
-
 
 	// This is the Sieve of Eratosthenes algorithm. We have
 	// an array with all numbers from 2 to n marked as true.
@@ -17,16 +19,12 @@ func Erat(n int) []bool {
 	// all of its multiples from the list, marking them as false.
 	// Then we move to the next integer in the list not false and
 	// repeate the steps.
-	for p:=2;p<n;p++ {
+	var largestNum int = int(math.Sqrt(float64(n)))
+	for p := 2; p < largestNum; p++ {
 		if x[p] == true {
-			start := int(math.Pow(float64(p),2))
-
-			if start < n {			
-				for j:=start;j<n;j+=p {
-				 x[j] = false
-				}
-			} else {
-				break
+			start := int(math.Pow(float64(p), 2))
+			for j := start; j < n; j += p {
+				x[j] = false
 			}
 		}
 	}
